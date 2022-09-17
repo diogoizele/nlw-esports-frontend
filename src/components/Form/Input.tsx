3af70b58ff850d9dcff2485252a6845eb3280d1e
Diogo Izele <diogo.izele@cwi.com.br>
2022-09-17 15:34:40 -0300

    removes autocomplete in input component

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -5,6 +5,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 export function Input({ ...props }: InputProps) {
   return (
     <input
+      autoComplete="off"
       {...props}
       placeholder="Selecione o game que deseja jogar"
       className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
